18c08e3e475ba02130078c2b2e92c196d42dd559
Braden Lamb <braden@int24.io>
2022-07-19 17:02:11 -0400

    fix: unencrypted regex

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const ENCRYPTED_REGEX = [/(dQw4w9WgXcQ:)([^.*['(.*)'\].*$][^"]*)/]
 
 export const UNENCRYPTED_REGEX = [
-    /[\w-]{24}\.[\w-]{6}\.[\w-]{27}/,
+    /[\w-]{24}\.[\w-]{6}\.[\w\-_]+/,
     /mfa\.[\w-]{84}/
 ]
